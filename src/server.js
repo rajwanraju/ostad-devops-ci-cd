0782e08724961c2efb6cee14af53ffdd718dbfbd
johnathanSSD <johnathan@seattlesoftwaredevelopers.com>
2025-08-23 12:00:34 +0600

    revert message to hello world

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello rajwan, Change in the code' });
+  res.json({ message: 'Hello World' });
 });
 
 let server;
